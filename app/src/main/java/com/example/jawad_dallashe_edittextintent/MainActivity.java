deb1262581d373ea21f8b19db34020481152d850
DESKTOP-FIA3LA0 <jawaddl2007@Gmail.com>
2024-04-11 13:32:35 +0300

    commit 1

--- app/src/main/java/com/example/jawad_dallashe_edittextintent/MainActivity.java
+++ app/src/main/java/com/example/jawad_dallashe_edittextintent/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.jawad_dallashe_edittextintent;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +17,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button Randomer, calculation, Delete ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Randomer = findViewById(R.id.Randomer);
+        calculation = findViewById(R.id.calculation);
+        Delete = findViewById(R.id.Delete);
+
+        Randomer.setBackgroundColor(Color.rgb(33, 128, 82));
+        calculation.setBackgroundColor(Color.rgb(33, 128, 82));
+        Delete.setBackgroundColor(Color.rgb(200, 207, 204));
     }
 }
